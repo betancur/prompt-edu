9b869d491e75484d0c3a26719451e494ad60f753
Jose Betancur <betancur@gmail.com>
2024-11-26 15:03:58 -0500

    fix en modo dark

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="mt-16 bg-white">
+    <footer className="mt-16 bg-white dark:bg-background">
       <div className="container mx-auto max-w-[1200px] py-12">
         <div className="flex flex-col items-center">
           <div className="flex flex-col md:flex-row gap-8 mb-4 items-center justify-center">
@@ -8,21 +8,21 @@ function Footer() {
               <img
                 src="/resources/Logo-Eafit.png"
                 alt="Universidad Eafit"
-                className="h-full w-auto object-contain"
+                className="h-full w-auto object-contain dark:invert dark:brightness-0 dark:contrast-200"
               />
             </div>
             <div className="h-[200px]">
               <img
                 src="/resources/Logo-GilbertoEcheverri.png"
                 alt="Corporación Gilberto Echeverri Mejia"
-                className="h-full w-auto object-contain"
+                className="h-full w-auto object-contain dark:invert dark:brightness-0 dark:contrast-200"
               />
             </div>
             <div className="h-[200px]">
               <img
                 src="/resources/Logo-Gobernacion.png"
                 alt="Gobernación de Antioquia"
-                className="h-full w-auto object-contain"
+                className="h-full w-auto object-contain dark:invert dark:brightness-0 dark:contrast-200"
               />
             </div>
           </div>
